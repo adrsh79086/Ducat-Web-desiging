78986f6fcdcdab3c71e64c650a6d5459a99e26cd
Adarsh Kuma <adkmr08@gmail.com>
2025-01-20 22:13:27 +0530

    practiceforJS

---
+++ pratice.js
@@ -0,0 +1,24 @@
+let score = 102;
+
+var a = "ADARSH";
+var b = 6;
+var c = 5;
+var d = 5;
+
+//console.log(a == b);
+
+console.log(a != b);
+console.log(a === b);
+
+///console.log(typeof(a));
+
+
+let abc;
+console.log(abc);
+
+
+let username = "Adarsh"
+
+let greeting = `hello${username} `
+
+console.log(greeting);
